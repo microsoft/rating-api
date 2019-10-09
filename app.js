c09e03bc8be577cd29c4d9828b987a823a3d0627
Ahmed Sabbour <103856+sabbour@users.noreply.github.com>
2019-10-09 16:22:31 -0700

    Autoloading data (#1)
    * Added autoloading of mock data

--- app.js
+++ app.js
@@ -34,6 +34,45 @@ var connectOptions = { useMongoClient: true, autoIndex: false};
 mongoose.connect(process.env.MONGODB_URI, connectOptions, function(error){
   if(!error){
     console.dir('CONNECTED TO ' + process.env.MONGODB_URI);
+    
+    var Rate = mongoose.model("Rate");
+    var Item = mongoose.model("Item");
+    var Site = mongoose.model("Site");
+
+    // Check if the items are empty, insert mock data
+    Item.count({}, function(err, c) {
+      if(c == 0) {
+        console.dir('No items found in the database. Loading data.');
+        var itemsMock = require('./data/items.json');
+        Item.collection.insertMany(itemsMock, function(err,r) {
+          if(err) {
+            console.error('Error inserting items: ' + err);
+          } else {
+            console.dir('Items loaded.');
+          }
+        });
+      } else {
+        console.dir( c + ' items found in the database. Skipping loading data.');
+      }
+    });
+
+    // Check if the sites are empty, insert mock data
+    Site.count({}, function(err, c) {
+      if(c == 0) {
+        console.dir('No sites found in the database. Loading data.');
+        var sitesMock = require('./data/sites.json');
+        Site.collection.insertMany(sitesMock, function(err,r) {
+          if(err) {
+            console.error('Error inserting sites: ' + err);
+          } else {
+            console.dir('Sites loaded.');
+          }
+        });
+      } else {
+        console.dir( c + ' sites found in the database. Skipping loading data.');
+      }
+    });
+
   }
 });
 
